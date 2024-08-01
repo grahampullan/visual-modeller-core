8bb08ed08d23fcd8b9b1693a494ce027f1de53cf
Graham Pullan <gp10006@cam.ac.uk>
2024-08-01 14:56:30 +0100

    renamed Connection to Socket

--- src/Socket.js
+++ src/Socket.js
@@ -1,9 +1,9 @@
-class Connection {
+class Socket {
     constructor(options) {
-        this.name = options.name || 'connection';
+        this.name = options.name || 'socket';
         this.state = options.state || {};
         this.linkId = options.linkId || '';
     }
 }
 
-export { Connection };
+export { Socket };
