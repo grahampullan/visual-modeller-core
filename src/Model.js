4cfe6100cb0427592da8268af8aea9c606766aae
Graham Pullan <gp10006@cam.ac.uk>
2024-08-14 13:59:13 +0100

    add and remove Logs from Model

--- src/Model.js
+++ src/Model.js
@@ -2,8 +2,10 @@ class Model {
     constructor(options) {
         this.nodes = options.nodes || [];
         this.links = options.links || [];
+        this.logs = options.logs || [];
         this.maxNodeId = 0;
         this.maxLinkId = 0;
+        this.maxLogId = 0;
     }
 
     getNodeId() {
@@ -28,6 +30,11 @@ class Model {
         this.links.push(link);
     }
 
+    addLog(log) {
+        log.id = this.getLogId();
+        this.logs.push(log);
+    }
+
     removeNode(node) {
         this.nodes = this.nodes.filter(n => n !== node);
     }
@@ -36,12 +43,20 @@ class Model {
         this.links = this.links.filter(l => l !== link);
     }
 
+    removeLog(log) {
+        this.logs = this.logs.filter(l => l !== log);
+    }
+
     removeNodeById(id) {
         this.nodes = this.nodes.filter(n => n.id !== id);
     }
 
     removeLinkById(id) {
         this.links = this.links.filter(l => l.id !== id);
+    }
+
+    removeLogById(id) {
+        this.logs = this.logs.filter(l => l.id !== id);
     }
 }
 
